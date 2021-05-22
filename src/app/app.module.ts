86f05dc70a252c2795f859987a197dfac2b0717b
Kristy Mae Almuete <kristyalmuete@gmail.com>
2021-05-22 22:45:18 +0800

    #14 Setup Initial Service Worker Functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from '@app/app-routing.module';
 import { CoreModule } from '@core/core.module';
 
 import { AppComponent } from '@app/app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -14,7 +16,13 @@ import { AppComponent } from '@app/app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   bootstrap: [AppComponent]
 })
